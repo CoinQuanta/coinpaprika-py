ff509bd86a9f948420f6650ab83d0c5a6efb1133
Darwin Shameran <dxsh@riseup.net>
2019-04-24 12:53:14 +0200

    Abstract requests.Response further, return a CoinpaprikaResponse obj instead

--- coinpaprika/common/request.py
+++ coinpaprika/common/request.py
@@ -1,5 +1,6 @@
 """HTTP request handling interface."""
 import logging
+from collections import namedtuple
 from typing import Dict
 
 import requests
@@ -8,6 +9,12 @@ from .ratelimit import rate_limited
 from .const import API_BASE_ENDPOINT
 
 class Request(requests.Session):
+
+    CoinpaprikaResponse = namedtuple("CoinpaprikaResponse", [
+        "content", "status_code", "headers", "url", "history",
+        "encoding", "reason", "cookies", "elapsed", "request", "json"
+    ])
+
     def __init__(self, debug: bool = False,
                  headers: Dict[str, str] = None, proxy: Dict[str, str] = None,
                  *args, **kwargs):
@@ -21,9 +28,26 @@ class Request(requests.Session):
             self.session.proxies.update(proxy)
 
     @rate_limited(10)
-    def request(self, method, path, *args, **kwargs) -> requests.models.Response:
+    def request(self, method, path, *args, **kwargs) -> CoinpaprikaResponse:
         """Send a HTTP request."""
         url: str = API_BASE_ENDPOINT + path
-        return super(Request, self).request(method, url, *args, **kwargs)
+        req: requests.models.Response = super(Request, self).request(method,
+                                                                     url,
+                                                                     *args,
+                                                                     **kwargs)
+
+        return Request.CoinpaprikaResponse(
+            content = req.content,
+            status_code = req.status_code,
+            headers = req.headers,
+            url = req.url,
+            history = req.history,
+            encoding = req.encoding,
+            reason = req.reason,
+            cookies = req.cookies,
+            elapsed = req.elapsed,
+            request = req.request,
+            json = req.json()
+        )
 
 #  vim: set ts=4 sw=4 tw=79 ft=python et :
