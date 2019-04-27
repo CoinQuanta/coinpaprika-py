7f6a034de5242175b1c9ade43fc684ba740601cb
Darwin Shameran <dxsh@riseup.net>
2019-04-27 16:16:19 +0200

    Use the same exception format for both exceptions

--- coinpaprika/common/exceptions.py
+++ coinpaprika/common/exceptions.py
@@ -2,21 +2,16 @@
 
 class CoinpaprikaException(Exception):
     """Base class from which all other exception classes extend from."""
+    def __init__(self, error_type: int, error_message: str):
+        error_str = "{}: {}".format(error_type, error_message)
+
+        super(CoinpaprikaException, self).__init__(error_str)
+        self.error_type = error_type
+        self.error_message = error_message
+
 
 class APIException(CoinpaprikaException):
     """Indicate exception along responses from Coinpaprika's API."""
-    def __init__(self, error_type: int, error_message: str):
-        """Initialize an instance of APIException.
-
-            Args:
-                error_type: HTTP status code.
-                error_message: Error message returned from Coinpaprika's API.
-            """
-        error_str = "{}: {}".format(error_type, error_message)
-
-        super(APIException, self).__init__(error_str)
-        self.error_type = error_type
-        self.error_message = error_message
 
 
 class ClientException(CoinpaprikaException):
